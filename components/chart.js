6284e87cba058a5129fce6d158cdf881f6786461
paranoiark <paranoiark@outlook.com>
2018-12-06 15:48:00 -0600

    updated code structure

---
+++ components/chart.js
@@ -0,0 +1,4 @@
+/// Resources:
+// 1. https://github.com/JesperLekland/react-native-svg-charts
+// 2. https://github.com/capitalone/react-native-pathjs-charts
+// 3. https://github.com/tomauty/react-native-chart
---
+++ components/meter.js
@@ -0,0 +1,6 @@
+// Resources:
+// 1. https://github.com/bartgryszko/react-native-circular-progress
+// 2. https://github.com/nerdyfactory/react-native-simple-gauge
+// 3. https://github.com/Reggino/react-svg-gauge
+// 4. https://github.com/palerdot/react-d3-speedometer
+// 5. https://github.com/arnthor3/react-liquidchart
