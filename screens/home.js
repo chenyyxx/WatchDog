0c7f25ea896ff6ca0d5dbb96c2f02653da045035
Francis1240 <31699006+Francis1240@users.noreply.github.com>
2018-12-09 14:20:50 -0600

    Update home.js

--- screens/home.js
+++ screens/home.js
@@ -1,29 +1,50 @@
 import React from "react";
-import { View, Text, Button, Flexbox } from "react-native";
+import { View, Text, Flexbox, StyleSheet } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import { Icon } from 'react-native-elements';
+import { Icon, Button } from 'react-native-elements';
 
 
 import Meter from "./components/meter"
 import Chart from "./components/chart"
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title : "Home",
+  static navigationOptions =({navigation})=> {
+    return {
+    headerTitle: "Home",
+    };
   };
+
 
 
   render() {
     return (
+      <View style={styles.container}>
         <View>
-            <Meter score={90} />
-            <Chart />
-            <Button
-                title = "Settings"
-                onPress = {() => this.props.navigation.navigate("Settings")}
-            />
+          <Meter score={90} />
         </View>
+        <View>
+          <Chart />
+        </View>
+        <View>
+          <Icon
+            name = 'settings'
+            title = "Settings"
+            onPress = {() => this.props.navigation.navigate("Settings")}
+          />
+        </View>
+      </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e6e6e6"
+    }
+});
+
+
+
 export default HomeScreen;
