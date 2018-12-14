e5979449fbb0553c9ab16ece0c29443004cd1f4c
paranoiark <paranoiark@outlook.com>
2018-12-14 01:35:40 -0600

    rendered markers onto the map

--- screens/map.js
+++ screens/map.js
@@ -1,55 +1,96 @@
 import React from "react";
-import {StyleSheet, View, Text, Alert} from "react-native";
+import {StyleSheet, View, Text, Alert, Dimensions} from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import Map from "./components/mapView"
 import { Location, Permissions } from 'expo'
 import MapMarker from "react-native-maps/lib/components/MapMarker";
+import { MapView } from "expo";
+
+const {width, height} = Dimensions.get('window');
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width/height;
+const LATITUDE_DELTA = 0.00722;
+const LONGITUDE_DELTA = LATITUDE_DELTA*ASPECT_RATIO;
+import * as Data2014 from '../2014.json';//21532
 
 
-class MapScreen extends React.Component {
+
+export default class MapScreen extends React.Component {
     constructor(props){
-        super(props);
+      super(props);
         this.state = {
-            latitude: 44.972,
-            longitude: -93.271,
-            error: null,
-            loading: true,
-        };
-    }
+            markers: MapScreen.renderList()
+        }
+    };
+
+    static renderList(){
+        let list=[];
+        for (let i = 0; i < 21532; i++) {
+            list.push({
+                key: i,
+                title: Data2014[i].Description,
+                coordinates:{
+                    latitude: Data2014[i].Latitude,
+                    longitude: Data2014[i].Longitude,
+                }
+            })
+        }
+        return list
+    };
+
 
     render() {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                    loading: false,
-                });
-            },
-            error => Alert.alert(error.message),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-        );
-        let region = {
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.00722,
-            longitudeDelta: 0.00321
-        };
-        let position = {
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-        };
         return (
             <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Map region={region} position={position}/>
+                <MapView
+                    style={styles.container}
+                    showsUserLocation={true}
+                    followUserLocation={true}
+                    initialRegion={{
+                        latitude: 44.940,
+                        longitude: -93.165,
+                        latitudeDelta: LATITUDE_DELTA,
+                        longitudeDelta: LONGITUDE_DELTA
+                    }}
+                >
+                    {this.state.markers.map((marker,index) => (
+                        <MapView.Marker
+                            key={index}
+                            coordinate={marker.coordinates}
+                            title={marker.title}
+                        />
+                    ))}
+                </ MapView>
             </View>
-        );
+        )
     }
 }
 
-
-
-
-
-export default MapScreen;
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+        height: "80%",
+        alignItems: 'center',
+    },
+    radius: {
+        height: 50,
+        width: 50,
+        borderRadius: 50/2,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(0,122,255,0.1)',
+        borderWidth: 1,
+        borderColor: 'rgba(0,122,255,0.3)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    marker: {
+        height: 20,
+        width: 20,
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 20/2,
+        overflow: 'hidden',
+        backgroundColor: '#007AFF',
+    },
+});
