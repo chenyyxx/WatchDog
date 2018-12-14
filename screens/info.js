2ac93210b89de274456f9c96ddb8ef44ee0af430
Francis1240 <31699006+Francis1240@users.noreply.github.com>
2018-12-14 17:44:36 -0600

    update info

--- screens/info.js
+++ screens/info.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image} from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+
 
 class InfoScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>My Settings Screen !!!</Text>
+      <View style={{marginTop: 50}}>
+        <Image source={require('../assets/icon.png')} style={{width: 200, height: 200}}/>
+        <Text style={{marginLeft:20, marginRight:0}}>By Francis Ma, Yuxiang Chen</Text>
+        <Text style={{marginTop:20, marginLeft:20, marginRight:0}}>Due to the limit access to crime data, we only included the data from December 2014 in Minneapolis. The locations of the incidents may not be accurate, and not all incidents are included.</Text>
       </View>
     );
   }
